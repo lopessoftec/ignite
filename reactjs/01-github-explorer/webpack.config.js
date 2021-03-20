b2dc1bfede75a2aef80fae97bd8df412fb974240
lopessoftec <lopessoftec@gmail.com>
2021-03-20 20:21:43 -0300

    criando estrutura, babel, webpack

---
+++ reactjs/01-github-explorer/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  // qual o arquivo principal
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  // qual arquivo irei gerar
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    //  array de regras
+    rules: [
+      {
+        // para saber se é js ou não
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
