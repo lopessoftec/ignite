4518f1210289125f53c475d77be07a4fd542fd74
lopessoftec <lopessoftec@gmail.com>
2021-04-11 21:05:04 -0300

    configurando mirageJS

--- reactjs/dtmoney/src/index.tsx
+++ reactjs/dtmoney/src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  routes(){
+    // fetch('http://localhost:3000/api/transactions') pois aqui tem esse 'api'
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Trasaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
