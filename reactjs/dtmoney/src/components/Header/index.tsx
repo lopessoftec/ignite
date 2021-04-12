4a22abc769123d66128d99c1d01fd7b536205903
lopessoftec <lopessoftec@gmail.com>
2021-04-11 22:44:42 -0300

    configirando modal de criação

--- reactjs/dtmoney/src/components/Header/index.tsx
+++ reactjs/dtmoney/src/components/Header/index.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header(){
+interface HeaderProps {
+    // é uma funçao e o retorno e vazio
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps){
+    
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
-                </button>
+                </button>                
             </Content>
         </Container>
     )
