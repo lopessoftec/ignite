f795a81740bb87c68a49a3a6654d3abb8f61c2cb
lopessoftec <lopessoftec@gmail.com>
2021-04-16 18:44:55 -0300

    static site generate (ssg)

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,9 +1,16 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
+
+// Client-side
+// Server-side
+// Static Site Generation
+
+// Conteudo (ssg)
+// Comentatios (client-side) só depois da pagina ter carregado
 
 interface HomeProps {
   product: {
@@ -37,7 +44,7 @@ export default function Home({product} : HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   // const price = await stripe.prices.retrieve('price_1IgwUCIwoS1x7pFktHek4Fo4', {
   //   expand: ['product']
   // })
@@ -54,6 +61,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
