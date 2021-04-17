3dfc293ae7cd7d2bf957373242290a65d972978c
lopessoftec <lopessoftec@gmail.com>
2021-04-17 18:36:19 -0300

    gerando sessão de checkout

--- reactjs/ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/ignews/src/components/SubscribeButton/index.tsx
@@ -1,14 +1,32 @@
+import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+//locais abaixo onde usuario não acessa
+//getServerSideProps (SSR)
+//getStaticProps (SsG)
+//api routes //quando acontece depois da pagina ser exibida
+
 export function SubscribeButton({priceId}: SubscribeButtonProps){
+    const [session] = useSession();
+
+    function handleSubscribe(){
+        if(!session){
+            signIn('github')
+            return;
+        }
+
+        // criação da checkout session
+    }
+
     return (
         <button 
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
             Subscribe now
         </button>
